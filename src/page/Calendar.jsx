2ff89617135facbf6643ca79f3059819e4865384
AngelicaMamiar <mamiarangelica@gmail.com>
2024-09-27 12:21:38 +0300

    first maj

--- src/page/Calendar.jsx
+++ src/page/Calendar.jsx
@@ -1,20 +1,17 @@
-import React, { act, useState } from "react";
+import React, { useState } from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import withDragAndDrop from "react-big-calendar/lib/addons/dragAndDrop";
 import "react-big-calendar/lib/addons/dragAndDrop/styles.css";
 import "react-big-calendar/lib/css/react-big-calendar.css";
-import './components/Component-Calendar-Css.css';
+import './components/Component-Calendar-Css.css';  // Assurez-vous que le CSS place correctement le calendrier
 import defaultEvents from "./components/DefaultEvents";
 import EventModal from "./components/EventModal";
-
-
 
 const DragAndDropCalendar = withDragAndDrop(Calendar);
 const localizer = momentLocalizer(moment);
 
 function MyCalendar() {
-
     const [events, setEvents] = useState(defaultEvents);
     const [SelectedEvents, setSelectedEvents] = useState(null);
 
@@ -22,9 +19,8 @@ function MyCalendar() {
         style: {
             backgroundColor: event.color,
         },
-        
-    })
-    
+    });
+
     const moveEvents = (data) => {
         const { start, end } = data;
         const updatedEvents = events.map((event) => {
@@ -39,56 +35,57 @@ function MyCalendar() {
         });
 
         setEvents(updatedEvents);
-    }
+    };
+
     const handleEventClick = (event) => {
         setSelectedEvents(event);
-
-    }
+    };
 
     const handleEventClose = () => {
         setSelectedEvents(null);
-    }
+    };
    
     return (
         <div className="Screen">
             <div className="toolbar">
                 <p>Calendar</p>
-
             </div>
 
-        <div className="calendar-container">
+            <div className="calendar-container">
+                <DragAndDropCalendar
+                    defaultDate={moment().toDate()}
+                    defaultView="month"
+                    events={events}
+                    localizer={localizer}
+                    resizable
+                    onEventDrop={moveEvents}
+                    onEventResize={moveEvents}
+                    onSelectEvent={handleEventClick}
+                    eventPropGetter={eventStyle}
+                    components={{
+                        toolbar: CustomToolbar,
+                    }}
+                    className="calendar"
+                />
+            </div>
 
-        </div>
-            <DragAndDropCalendar
-                defaultDate={moment().toDate()}
-                defaultView='month'
-                events={events}
-                localizer={localizer}
-                resizable
-                onEventDrop={moveEvents}
-                onEventResize={moveEvents}
-                onSelectEvent={handleEventClick}
-                eventPropGetter={eventStyle}
-                components={{
-                    toolbar: CustomToolbar,
-                }}
-                className='calendar'
-            />
             {SelectedEvents && (
                 <EventModal 
-            event={SelectedEvents} 
-            onClose={handleEventClose} 
-            />
+                    event={SelectedEvents} 
+                    onClose={handleEventClose} 
+                />
             )}
         </div>
     );
 }
 
-const CustomToolbar = ({label, onView, onNavigate, views}) => {
-   const handleNavigate = (action) => {
-       onNavigate(action);
-       }
-       const [itemText, setItemText] = useState('month');
+const CustomToolbar = ({ label, onView, onNavigate, views }) => {
+    const [itemText, setItemText] = useState('month'); // Définit la vue par défaut (month)
+
+    const handleViewChange = (view) => {
+        onView(view); // Change la vue actuelle
+        setItemText(view); // Met à jour le texte du bouton avec la vue sélectionnée
+    };
 
     return (
         <div className="toolbar-container">
@@ -96,28 +93,32 @@ const CustomToolbar = ({label, onView, onNavigate, views}) => {
 
             <div className="topDirectory">
                 <div className="dropdown">
-                    <button className='btn btn-secondary dropdown-toggle' type='button' id='dropdownMenuButton' data-bs-toogle='dropdown' aria-expanded='false'>
-                         {itemText}
+                    <button 
+                        className="btn btn-secondary dropdown-toggle" 
+                        type="button" 
+                        id="dropdownMenuButton" 
+                        data-bs-toggle="dropdown" // Correction ici
+                        aria-expanded="false"
+                    >
+                        {itemText} {/* Affiche la vue sélectionnée */}
                     </button>
-                    <ul className='dropdown-menu' aria-labelledby='dropdownMenuButton'>
-                        {views.map((view,index) =>(
-                            <div kay={index}>
-                                <li>
-                                    <button className='dropdown-item' onClick={()=>onView(view)}>{view}</button>
-                                </li>
-                               
 
-                            </div>
-                        ) )
-
-                        }
+                    <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        {views.map((view, index) => (
+                            <li key={index}>
+                                <button 
+                                    className="dropdown-item" 
+                                    onClick={() => handleViewChange(view)}
+                                >
+                                    {view}
+                                </button>
+                            </li>
+                        ))}
                     </ul>
-                        
                 </div>  
-
             </div>
         </div>
-        
-    )
-}
+    );
+};
+
 export default MyCalendar;
